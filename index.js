384a7e436d3247b461f75d1048e3a156b5991de3
firoj10 <mdfirojhasan.info@gmail.com>
2023-06-01 21:52:21 +0600

    part-2 commit

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+
 
 
 // middlewire
 app.use(cors())
 app.use(express.json())
 
+const verifyJWT = (req, res, next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+  //bearer token
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4gqmum7.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -29,17 +49,114 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartsCollection = client.db("bistroDb").collection("carts");
 
+    app.post('/jwt',(req, res)=>{
+      const user =  req.body;
+      const token  = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token})
+    })
+
+
+    const verifyAdmin = async(req, res, next)=>{
+      const email = req.decoded.email;
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      if(user?.role !== 'admin'){
+        return res.status(403).send({error: true, message: 'forbedent mesage'});
+      }
+      next();
+    }
+
+ //user related api
+ app.get('/users', verifyJWT, verifyAdmin, async(req, res)=>{
+  const result = await usersCollection.find().toArray();
+  res.send(result)
+})
+
+ app.post('/users', async(req, res)=>{
+  const user = req.body;
+  const query = {email: user.email}
+  const existingUser = await usersCollection.findOne(query);
+  if(existingUser){
+    return res.send({message: 'user alredy exists'})
+  }
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+})
+
+//security layer veryfyjwt
+// email same
+// chehk admin
+app.get('/users/admin/:email', verifyJWT , async (req, res)=>{
+  const email = req.params.email;
+  console.log(email, req.decoded.email)
+  if(req.decoded.email != email){
+    res.send({admin: false})
+  }
+  const query = {email: email}
+  const user = await usersCollection.findOne(query);
+  console.log(user)
+  const result = {admin: user?.role === 'admin'}
+  res.send(result);
+})
+
+
+app.patch('/users/admin/:id', async (req, res)=>{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)}
+  const updateDoc ={
+    $set:{
+      role:'admin'
+    },
+  };
+  const result= await usersCollection.updateOne(filter, updateDoc);
+  res.send(result)
+})
+    //menu related api
     app.get('/menu', async(req, res)=>{
     const result = await menuCollection.find().toArray();
     res.send(result)
 })
 
+
+//review related api
     app.get('/reviews', async(req, res)=>{
     const result = await reviewCollection.find().toArray();
     res.send(result)
+})
+
+//cart collection apis
+
+app.get('/carts', verifyJWT, async(req, res)=>{
+  const email = req.query.email;
+  if(!email){
+    res.send([])
+  }
+  const decodedEmail = req.decoded.email;
+  if(email !== decodedEmail){
+    return res.status(403).send({error: true, message: 'providen access'});
+  }
+  const query = {email: email};
+  const result = await cartsCollection.find(query).toArray();
+  res.send(result)
+})
+
+app.post('/carts', async(req, res)=>{
+  const item = req.body;
+  console.log(item)
+  const result = await cartsCollection.insertOne(item);
+  res.send(result);
+})
+ 
+app.delete('/carts/:id', async(req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await cartsCollection.deleteOne(query);
+  res.send(result);
 })
 
     // Send a ping to confirm a successful connection
@@ -51,9 +168,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
 
 
 app.get('/', (req, res)=>{
